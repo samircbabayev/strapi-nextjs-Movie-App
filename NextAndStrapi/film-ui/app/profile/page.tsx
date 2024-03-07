36d05eb3f1fd3aed436186a9a8f17410834bb5ff
Samir <samircbabayev@gmail.com>
2024-03-07 16:52:11 +0530

    profile changed

--- NextAndStrapi/film-ui/app/profile/page.tsx
+++ NextAndStrapi/film-ui/app/profile/page.tsx
@@ -1,11 +1,25 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useAuth } from "@/utils/authContext";
 import Image from "next/image";
+import axiosInstance from "@/utils/axiosConfig";
 
 const Profile = () => {
   const { user } = useAuth();
   console.log(user);
+
+  const [data, setData] = useState(null);
+  console.log(data);
+
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const response = await axiosInstance.get(`/Users/me?populate=*`);
+        setData(response.data);
+      } catch (error) {}
+    };
+    fetch();
+  }, []);
 
   return (
     <div className="px-20 h-[90vh] py-10">
@@ -14,7 +28,17 @@ const Profile = () => {
         <div>
           <p>Username: {user.username}</p>
           <p>Email: {user.email}</p>
-          {/* Add additional profile information here */}
+          {/* {data && data.pic.formats.medium.url && (
+            <div>
+              <p>Profile Picture:</p>
+              <Image
+                src={data.pic.formats.medium.url}
+                alt="Profile Picture"
+                width={200}
+                height={200}
+              />
+            </div>
+          )} */}
         </div>
       ) : (
         <p>Loading...</p>
